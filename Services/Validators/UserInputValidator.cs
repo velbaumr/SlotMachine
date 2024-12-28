ddfe7c00a10db0a4c44ff1b599d42d2f512c2c31
velbaumr <58743184+velbaumr@users.noreply.github.com>
2024-12-28 13:40:13 +0200

    added input validation

--- Services/Validators/UserInputValidator.cs
+++ Services/Validators/UserInputValidator.cs
@@ -7,6 +7,14 @@ public class UserInputValidator : AbstractValidator<UserInput>
 {
     public UserInputValidator()
     {
+        RuleFor(x => x.SpinCount)
+            .GreaterThan(0);
         
+        RuleFor(x => x.StartBalance)
+            .GreaterThan(0)
+            .GreaterThanOrEqualTo(x => x.Bet);
+        
+        RuleFor(x => x.Bet)
+            .GreaterThan(0);
     }
 }
